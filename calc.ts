ba59ef2166fb93fc0f16f6ec20112a9df92d6d76
Vladislav Klepikov <ya.losyash@yandex.ru>
2024-09-23 18:00:13 +0300

    add some logic

--- calc.ts
+++ calc.ts
@@ -1,6 +1,17 @@
 function calc(example: string): number {
+  if (example.search(/[^1-9() +-\/*]/g) !== -1) {
+    console.log();
+    throw new Error("Некорректное значение");
+  }
 
-  // let arrExample: string[] = example;
-return 0;
+  let strExample: string = example.replace(',', '.').replaceAll(' ', '');
+
+
+
+  console.log(strExample);
+  return 0;
 }
-console.log(calc('2 * (5 - (1 + 5) / 6)')) // 8
+
+console.log(calc("2,2 * (5 - (1 + 5) / 6)")); // 8
+// a+b*c-d/e+(f-g*(j+k))-i*(x-z)
+// 363636+484884*362662-744774/2626626+(252662-47773*(362662+466363))-277373*(3737-27272)
