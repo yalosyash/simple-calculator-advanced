49d544b527fc1da7a0b00acde5dd361a05f0e186
Vladislav Klepikov <ya.losyash@yandex.ru>
2024-10-04 13:44:36 +0300

    update README.md / calc.spec.ts

--- src/calc.spec.ts
+++ src/calc.spec.ts
@@ -1,14 +1,8 @@
 import { expect } from "@jest/globals";
 import { calc } from "./calc";
+
 describe("calc", function () {
-  it("should return 5", function () {
-    expect(calc("3+2")).toBe(5);
-  });
-  it("should return -24812275.83587205", function () {
-    expect(
-      calc("3636+4848*3626-7447/2626+(2526-4777*(3626+4663))-2773*(3737-2727)")
-    ).toBe(-24812275.83587205);
-  });
+  //Негативные проверки-------------------------------------------------------------------
   it("should return Error", function () {
     expect(() => {
       calc("zxc");
@@ -24,23 +18,34 @@ describe("calc", function () {
       calc("4 ) + 42");
     }).toThrow();
   });
-  it("should return -1", function () {
-    expect(calc("-3+2")).toBe(-1);
-  });
   it("should return Error", function () {
     expect(() => {
-      calc("(3+2()");
+      calc("(4 + 42( )");
     }).toThrow();
   });
   it("should return Error", function () {
     expect(() => {
-      calc("3+(-2-)");
+      calc("3 + ( - 2 - )");
     }).toThrow();
   });
   it("should return Error", function () {
     expect(() => {
       calc("(3+2)()");
     }).toThrow();
+  });
+
+  // Позитивные проверки-------------------------------------------------------------------
+  it("should return 5", function () {
+    expect(calc("3+2")).toBe(5);
+  });
+  it("should return -24812275.83587205", function () {
+    expect(
+      calc("3636+4848*3626-7447/2626+(2526-4777*(3626+4663))-2773*(3737-2727)")
+    ).toBe(-24812275.83587205);
+  });
+
+  it("should return -1", function () {
+    expect(calc("-3+2")).toBe(-1);
   });
   it("should return 9", function () {
     expect(calc("(5 + (3+2)) - 1")).toBe(9);
